5ef84a0a873cfa216cb64182c4d6a75973408160
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-20 08:17:30 -0500

    Update MealViewController.swift

--- Food Tracker App/FoodTracker/FoodTracker/MealViewController.swift
+++ Food Tracker App/FoodTracker/FoodTracker/MealViewController.swift
@@ -1,10 +1,4 @@
-//
-//  MealViewController.swift
-//  FoodTracker
-//
-//  Created by Jane Appleseed on 10/17/16.
-//  Copyright © 2016 Apple Inc. All rights reserved.
-//
+
 
 import UIKit
 import os.log
