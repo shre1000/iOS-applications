97a73171e372adb004306f13981371c4817301ea
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-20 08:34:07 -0500

    Update Brand+CoreDataProperties.swift

--- Core Data App/Brand+CoreDataProperties.swift
+++ Core Data App/Brand+CoreDataProperties.swift
@@ -1,10 +1,4 @@
-//
-//  Brand+CoreDataProperties.swift
-//  CoreDataOlemissDemo
-//
-//  Created by Morgan Dock on 4/5/17.
-//  Copyright © 2017 Morgan Dock. All rights reserved.
-//
+
 
 import Foundation
 import CoreData
