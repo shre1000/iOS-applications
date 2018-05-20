a36827cc5699b80cd55bb75b8ca3b5cf31aeb727
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-19 21:35:26 -0500

    Add files via upload

---
+++ Tip Calculator/Tip Calculator/Tip/Tip/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Yixin Chen on 2/27/17.
+//  Copyright © 2017 University of Mississippi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // select inputTextField so keypad displays when the view loads
+//        inputTe
+    }
+
+}
+
