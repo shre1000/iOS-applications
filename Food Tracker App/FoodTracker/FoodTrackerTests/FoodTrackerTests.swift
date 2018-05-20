93d14098a0c13f8486597b99bdf024d820fcf1bb
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-19 22:32:00 -0500

    Update FoodTrackerTests.swift

--- Food Tracker App/FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
+++ Food Tracker App/FoodTracker/FoodTrackerTests/FoodTrackerTests.swift
@@ -1,10 +1,3 @@
-//
-//  FoodTrackerTests.swift
-//  FoodTrackerTests
-//
-//  Created by Jane Appleseed on 10/17/16.
-//  Copyright © 2016 Apple Inc. All rights reserved.
-//
 
 import XCTest
 @testable import FoodTracker
