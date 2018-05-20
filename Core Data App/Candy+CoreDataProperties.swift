a33c444b41e115683b7c46651575e6d1c4c6b66c
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-20 08:34:48 -0500

    Update Candy+CoreDataProperties.swift

--- Core Data App/Candy+CoreDataProperties.swift
+++ Core Data App/Candy+CoreDataProperties.swift
@@ -1,10 +1,3 @@
-//
-//  Candy+CoreDataProperties.swift
-//  CoreDataOlemissDemo
-//
-//  Created by Morgan Dock on 4/5/17.
-//  Copyright © 2017 Morgan Dock. All rights reserved.
-//
 
 import Foundation
 import CoreData
