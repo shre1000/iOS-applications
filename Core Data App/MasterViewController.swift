d1a5ceea4dc785956f8940792c56ce146d3c9d18
Shre1000 <31860419+shre1000@users.noreply.github.com>
2018-05-20 08:35:55 -0500

    Update MasterViewController.swift

--- Core Data App/MasterViewController.swift
+++ Core Data App/MasterViewController.swift
@@ -1,10 +1,4 @@
-//
-//  MasterViewController.swift
-//  CoreDataOlemissDemo
-//
-//  Created by Morgan Dock on 4/2/17.
-//  Copyright © 2017 Morgan Dock. All rights reserved.
-//
+
 
 import UIKit
 import CoreData
